97d85d51846a08633a940834f4428af5d35a523f
quinn woodhouse <qpwoodhouse@gmail.com>
2023-10-31 13:14:40 -0600

    first class functionslab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+  function createFareMultiplier(multiply){
+    return function(value){
+        return value * multiply
+
+    }
+  }
+  const fareDoubler = createFareMultiplier(2)
+  const fareTripler = createFareMultiplier(3)
+
+  function selectDifferentDrivers(drivers, selectFunction) {
+    return selectFunction(drivers);
+  }
+  const firstTwo = selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+  const lastTwo = selectDifferentDrivers(drivers, returnLastTwoDrivers);
+
+  console.log(firstTwo)
+  console.log(lastTwo)
